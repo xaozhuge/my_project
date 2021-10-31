07d0a52e5f51d18ee9a4d0b8b79e3d5f91467626
zzz <xaozhuge@163.com>
2021-10-31 16:48:15 +0800

    测试

--- Application/Home/Controller/TestController.class.php
+++ Application/Home/Controller/TestController.class.php
@@ -5,4 +5,18 @@ namespace Home\Controller;
  */
 class TestController extends Controller {
 
+	public function test(){
+		$start_date = '2021-09-10';
+		$end_date   = $this->getModel('Date')->getNumDaysAfter(30);
+		$range_date = $this->getModel('Date')->getDateRange($start_date, $end_date);
+		$content    = '';
+		foreach ($range_date as $v) {
+			$remind_info = $this->getModel('RemindDate')->getRemindByDate($v);
+			$content    .= $this->getModel('RemindDate')->formatRemindContent($remind_info);
+		}
+		$title = $start_date. '到'. $end_date;
+		$toemail = 'xaozhuge@163.com';
+		$res = $this->getModel('Email')->sendEmail($toemail, $title, $content);
+		p($res);
+	}
 }
