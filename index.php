0c97c76ab223451d2b0daf7a8735f8f605589d86
zzz <xaozhuge@163.com>
2021-10-31 17:07:02 +0800

    相对路径改为绝对路径

--- index.php
+++ index.php
@@ -23,9 +23,9 @@ define('APP_DEBUG', true);
 define('BIND_MODULE', 'Home');
 
 // 定义应用目录
-define('APP_PATH', './Application/');
+define('APP_PATH',__DIR__.'/Application/');
 
 // 引入ThinkPHP入口文件
-require './ThinkPHP/ThinkPHP.php';
+require __DIR__.'/ThinkPHP/ThinkPHP.php';
 
 // 亲^_^ 后面不需要任何代码了 就是如此简单
