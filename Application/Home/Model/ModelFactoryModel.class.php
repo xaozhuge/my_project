bd5743bc1a54f724eb70c59691727eb1a86482d1
zzz <xaozhuge@163.com>
2022-03-23 17:06:11 +0800

    通过表名创建Model

--- Application/Home/Model/ModelFactoryModel.class.php
+++ Application/Home/Model/ModelFactoryModel.class.php
@@ -32,11 +32,29 @@ class ModelFactoryModel{
 			}
 		}
 
+		// 通过表名创建
+		if($model == null){
+			$model = static::createModelByTable($modelName);
+		}
+
 		if($model == null){
 			echo $modelClass.' is not find';exit;
 		}
 		static::$models[$modelName] = $model;
 		return $model;
+	}
+
+	/**
+	 * [createModelByTable 通过表名创建model]
+	 * @author zzz
+	 * @DateTime 2022-03-23T11:57:37+0800
+	 */
+	public static function createModelByTable($modelName){
+		list($db, $table) = explode('.', $modelName);
+		//如果$table为空
+		if(empty($table)) return null;
+		//如果数据库配置不为空
+		if(!empty(C($db))) return new BModel($modelName); 
 	}
 	
 }
