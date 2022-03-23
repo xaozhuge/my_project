831267504ef83b6d7ccf73a7e0ccb52cabdeff89
zzz <zzz@qimingpian.com>
2022-03-23 11:55:37 +0800

    默认数据库配置

--- Application/Home/Conf/db.php
+++ Application/Home/Conf/db.php
@@ -9,6 +9,17 @@ $config	=	 array(
     'db_port' => C('DB_PORT'),
     'db_name' => C('DB_NAME'),
     'db_charset' => 'utf8mb4',
+
+    //默认数据库
+    'DEFAULT'=>array(
+	    'db_type' => 'mysql',
+	    'db_host' => C('DB_HOST'),
+	    'db_user' => C('DB_USER'),
+	    'db_pwd'  => C('DB_PWD'),
+	    'db_port' => C('DB_PORT'),
+	    'db_name' => C('DB_NAME'),
+	    'db_charset' => 'utf8mb4',
+    ),
 );
 
 return $config;
