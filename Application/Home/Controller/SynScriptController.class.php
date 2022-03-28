1ae762d35590214ff229ca2a05ac0e78c618d5e6
zzz <xaozhuge@163.com>
2022-03-28 20:31:57 +0800

    同步近5分钟内更新过的数据表

--- Application/Home/Controller/SynScriptController.class.php
+++ Application/Home/Controller/SynScriptController.class.php
@@ -14,8 +14,27 @@ class SynScriptController extends ScriptController {
      * @DateTime 2022-03-28T20:23:29+0800
      */
     public function synTable(){
+        #1. 定义时间和数据库
         $limit_time = date('Y-m-d H:i:s', strtotime(" -5 minutes"));
-        
+        $database   = 'zzz';
+        #2. 数据表列表
+        $sql  = "select  `TABLE_NAME`  from `information_schema`.`TABLES` where `information_schema`.`TABLES`.`TABLE_SCHEMA` = '{$database}' ";
+        $list = $this->getModel('ScriptLog')->query($sql);
+
+        #3. 判断时间
+        foreach ($list as  $v) {
+            $table_name  = $v['table_name'];
+            #获取最新时间
+            $update_time = $this->getModel('DEFAULT.'. $table_name)
+                ->order('update_time desc')
+                ->getField('update_time');
+            if($update_time >= $limit_time){
+
+            }else{
+
+            }
+        }
+        $this->scriptEnd();
     }
 
 
