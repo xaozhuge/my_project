7c76924e5a68acbc0360a6096c65f4108fb9ff7e
zzz <xaozhuge@163.com>
2022-03-28 20:45:13 +0800

    同步近5分钟内更新过的数据表

--- Application/Home/Controller/SynScriptController.class.php
+++ Application/Home/Controller/SynScriptController.class.php
@@ -24,7 +24,7 @@ class SynScriptController extends ScriptController {
         #3. 判断时间
         foreach ($list as  $v) {
             $table_name  = $v['table_name'];
-            #获取最新时间
+            #获取同步表的最新时间
             $update_time = $this->getModel('DEFAULT.'. $table_name)
                 ->order('update_time desc')
                 ->getField('update_time');
@@ -32,7 +32,16 @@ class SynScriptController extends ScriptController {
                 $this->output('数据不更新', $table_name, $update_time);
                 continue;
             }
-            
+
+            #获取被同步表的最新时间
+            $update_time_syn = $this->getModel('DEFAULT.'. $table_name)
+                ->order('update_time desc')
+                ->getField('update_time');
+            if($update_time_syn == $update_time){
+                $this->output('最新时间相同不更新', $table_name, $update_time);
+                continue;
+            }
+            $this->output('数据更新', $table_name, $update_time, $update_time_syn);
         }
         $this->scriptEnd();
     }
