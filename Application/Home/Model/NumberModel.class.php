fee9eaab8c8b0edae7976d2e3976f39a7f599f20
zzz <xaozhuge@163.com>
2022-03-29 10:55:24 +0800

    数字转化成汉字

--- Application/Home/Model/NumberModel.class.php
+++ Application/Home/Model/NumberModel.class.php
@@ -22,4 +22,35 @@ class NumberModel{
         return $num_value;
     }
 
+    /**
+     * [numToWord 数字转化成汉字]
+     * @author zzz
+     * @DateTime 2022-03-29T10:54:44+0800
+     */
+    public function numToWord($num){
+        $map_num_word = array(
+            '零', '一', '二', '三', '四', '五', '六', '七', '八', '九'
+        );
+        $map_digit_word = array(
+            '', '十', '百', '千'
+        );
+        $num = strval($num);
+        $num_length = strlen($num);
+        $num_list = str_split($num, 1);
+        $num_list = array_reverse($num_list);
+        foreach ($num_list as $k => $v) {
+            #1. 数字转化成汉字
+            $v = $map_num_word[$v];
+            #2. 位数 
+            $digit = $map_digit_word[$k];
+            #3. 数字和位数拼接
+            $v = $v. $digit;
+            $word_list[] = $v;
+        }
+        $word_list = array_reverse($word_list);
+        $res = implode('', $word_list);
+        return $res;
+
+    }
+
 }
